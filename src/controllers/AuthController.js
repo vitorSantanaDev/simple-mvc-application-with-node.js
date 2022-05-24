9b4ec6c1f6afdbebf2036c24e64677ecb8bd840a
Vitor Santana <vitor@buildbox.com.br>
2022-05-24 04:12:15 -0700

    feat: :sparkles: created simple user authentication

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -7,37 +7,62 @@ export class AuthController {
 		res.render('auth/login')
 	}
 
+	static async loginPost(req, res) {
+		try {
+			const { email, password } = req.body
+			const user = await User.findOne({ where: { email } })
+			if (!user) {
+				req.flash('error', 'Email ou senha são inválidos')
+				res.render('auth/login')
+				return
+			}
+
+			const passwordMatch = bcrypt.compareSync(password, user.password)
+			if (!passwordMatch) {
+				req.flash('error', 'Email ou senha são inválidos')
+				res.render('auth/login')
+				return
+			}
+
+			req.session.userId = user.id
+			req.session.save(() => {
+				res.redirect('/')
+			})
+		} catch (error) {
+			errorMessage(error)
+		}
+	}
+
 	static async register(req, res) {
 		res.render('auth/register')
 	}
 
 	static async registerPost(req, res) {
-		const { name, email, password, confirmPassword } = req.body
-
-		const checkIfUserExists = await User.findOne({ where: { email } })
-
-		if (checkIfUserExists) {
-			req.flash('error', 'Email já cadastrado')
-			res.render('auth/register')
-			return
-		}
-
-		if (password !== confirmPassword) {
-			req.flash('error', 'As senhas não conferem, tente novamente')
-			res.render('auth/register')
-			return
-		}
-
-		const salt = bcrypt.genSaltSync(10)
-		const hashedPassword = bcrypt.hashSync(password, salt)
-
-		const user = {
-			name,
-			email,
-			password: hashedPassword
-		}
-
 		try {
+			const { name, email, password, confirmPassword } = req.body
+			const checkIfUserExists = await User.findOne({ where: { email } })
+
+			if (checkIfUserExists) {
+				req.flash('error', 'Email já cadastrado')
+				res.render('auth/register')
+				return
+			}
+
+			if (password !== confirmPassword) {
+				req.flash('error', 'As senhas não conferem, tente novamente')
+				res.render('auth/register')
+				return
+			}
+
+			const salt = bcrypt.genSaltSync(10)
+			const hashedPassword = bcrypt.hashSync(password, salt)
+
+			const user = {
+				name,
+				email,
+				password: hashedPassword
+			}
+
 			const createdUser = await User.create(user)
 			req.session.userId = createdUser.id
 			req.flash('success', 'Usuário Cadastrado com sucesso!')
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,6 +7,7 @@ authRouter.get('/login', AuthController.login)
 authRouter.get('/register', AuthController.register)
 authRouter.get('/logout', AuthController.logout)
 
+authRouter.post('/login', AuthController.loginPost)
 authRouter.post('/register', AuthController.registerPost)
 
 export default authRouter
